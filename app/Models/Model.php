8417ec273d01003d737ec59b5d2ef87c91f5b7fd
Mike <support@midreg.com>
2020-04-13 09:08:05 +0300

    updated model

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -5,19 +5,51 @@ use PDOException;
 
 class Model
 {
-    public static function delete($table)
+    public static function delete($table, $id)
     {
-
+        return self::delete_where($table, [
+            'id' => $id
+        ]);
     }
 
-    public static function insert($table)
+    public static function delete_where($table, $where)
     {
+        $column = array_keys($where)[0];
+        $value = $where[0];
 
+        try {
+            $sql = "DELETE FROM $table WHERE $column = ?";
+
+            $prepare = Db::conn()->prepare($sql);
+            $prepare->execute([$value]);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
     }
 
-    public static function select($table, $value)
+    public static function insert($table, $data)
     {
-        return self::select_by($table, 'id', $value);
+        try {
+            $columns = implode(',', array_keys($data));
+
+            $questions = '?';
+            for($i=1;$i<count($data); $i++) {
+                $questions .= ',?';
+            }
+
+            $sql = "INSERT INTO $table ($columns) VALUES($questions)";
+
+            $prepare = Db::conn()->prepare($sql);
+            $prepare->execute();
+            return Db::conn()->lastInsertId();
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function select($table, $id)
+    {
+        return self::select_where($table, 'id', $id);
     }
 
     public static function select_all($table)
@@ -31,10 +63,13 @@ class Model
         }
     }
 
-    public static function select_by($table, $column_name, $value)
+    public static function select_where($table, $where)
     {
+        $column = array_keys($where)[0];
+        $value = $where[0];
+
         try {
-            $prepare = Db::conn()->prepare("SELECT * FROM $table WHERE $column_name = ?");
+            $prepare = Db::conn()->prepare("SELECT * FROM $table WHERE $column = ?");
             $prepare->execute([$value]);
             return $prepare->fetch();
         } catch (PDOException $e) {
